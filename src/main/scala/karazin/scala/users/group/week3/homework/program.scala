cff4bd71cf81baf3c228979b1c767ca3c505992c
Sviatoslav Siatynia <svyatiks0304@gmail.com>
2024-05-17 16:55:25 +0300

    Week-3

--- src/main/scala/karazin/scala/users/group/week3/homework/program.scala
+++ src/main/scala/karazin/scala/users/group/week3/homework/program.scala
@@ -1,27 +1,95 @@
-//package karazin.scala.users.group.week3.homework
-//
-//// Do not forget to import custom implementation
-//import karazin.scala.users.group.week3.homework.adt.*
-//import karazin.scala.users.group.week3.homework.model.*
-//import karazin.scala.users.group.week3.homework.services.*
-//
-//object program:
-//
-//  /*
-//    Replace `ErrorOr[List[PostView]] result type with `ErrorOrT[Future, List[PostView]]
-//    Provide the implementation for the service
-//   */
-//  def getPostsViews(apiKey: String)(commentsFilter: List[Comment] => Boolean,
-//                                    likesFilter: List[Like] => Boolean): ErrorOr[List[PostView]] = ???
-//
-//  /* 
-//    Replace `ErrorOr[PostView] result type with `ErrorOrT[Future, PostView]
-//    Provide the implementation for the service
-//  */
-//  def getPostView(post: Post)(commentsFilter: List[Comment] => Boolean,
-//                              likesFilter: List[Like] => Boolean): ErrorOr[PostView] = ???
-//
-//
-//
-//
-//
+package karazin.scala.users.group.week3.homework
+
+// Do not forget to import custom implementation
+import karazin.scala.users.group.week3.Functor
+import karazin.scala.users.group.week3.future.given
+import karazin.scala.users.group.week3.homework.adt.*
+import karazin.scala.users.group.week3.homework.model.*
+import karazin.scala.users.group.week3.homework.model.Post.PostId
+import karazin.scala.users.group.week3.homework.model.User.UserId
+import karazin.scala.users.group.week3.homework.services.*
+
+import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService, Future}
+import scala.concurrent.ExecutionContext.global
+import scala.util.{Failure, Success}
+
+object program extends App:
+
+  val singleThreadPoolContext1: ExecutionContextExecutorService =
+    ExecutionContext.fromExecutorService(Executors.newSingleThreadExecutor())
+
+  val fixedTreadPoolContext1: ExecutionContextExecutorService =
+    ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(10))
+
+  val singleThreadPoolContext2: ExecutionContextExecutorService =
+    ExecutionContext.fromExecutorService(Executors.newSingleThreadExecutor())
+
+  val fixedTreadPoolContext2: ExecutionContextExecutorService =
+    ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(10))
+
+  given ExecutionContext = ExecutionContext.global
+
+  /*
+    Replace `ErrorOr[List[PostView]] result type with `ErrorOrT[Future, List[PostView]]
+    Provide the implementation for the service
+   */
+  def getPostViews(post: Post)(commentsFilter: List[Comment] => Boolean,
+                              likesFilter: List[Like] => Boolean)
+                 (using ExecutionContext): ErrorOrT[Future, List[PostView]] =
+    println(s"Main thread: ${Thread.currentThread().getName}")
+
+    val getCommentsService = getComments(post.postId)(using fixedTreadPoolContext1)
+    val getLikesService = getLikes(post.postId)(using fixedTreadPoolContext1)
+    val getSharesService = getShares(post.postId)(using fixedTreadPoolContext1)
+
+    for
+      comments <- getCommentsService
+      if commentsFilter(comments)
+      likes <- getLikesService
+      if likesFilter(likes)
+      shares <- getSharesService
+    yield List(PostView(post, comments, likes, shares))
+
+
+  getPostViews(Post(UserId.generate, PostId.generate))(_ => true, _ => true).value.onComplete {
+    case Success(value)     => println(s"Completed with $value")
+    case Failure(exception) => println(s"Failed with $exception")
+  }
+
+  Thread.sleep(10000)
+
+  singleThreadPoolContext1.shutdown()
+  fixedTreadPoolContext1.shutdown()
+
+  /*
+    Replace `ErrorOr[PostView] result type with `ErrorOrT[Future, PostView]
+    Provide the implementation for the service
+  */
+  def getPostView(post: Post)(commentsFilter: List[Comment] => Boolean,
+                              likesFilter: List[Like] => Boolean)
+                 (using ExecutionContext): ErrorOrT[Future, PostView] =
+    println(s"Main thread: ${Thread.currentThread().getName}")
+
+    val getCommentsService = getComments(post.postId)(using fixedTreadPoolContext2)
+    val getLikesService = getLikes(post.postId)(using fixedTreadPoolContext2)
+    val getSharesService = getShares(post.postId)(using fixedTreadPoolContext2)
+
+    for
+      comments <- getCommentsService
+      if commentsFilter(comments)
+      likes <- getLikesService
+      if likesFilter(likes)
+      shares <- getSharesService
+    yield PostView(post, comments, likes, shares)
+
+
+  getPostView(Post(UserId.generate, PostId.generate))(_ => true, _ => true).value.onComplete {
+    case Success(value)     => println(s"Completed with $value")
+    case Failure(exception) => println(s"Failed with $exception")
+  }
+
+  Thread.sleep(10000)
+
+  singleThreadPoolContext2.shutdown()
+  fixedTreadPoolContext2.shutdown()
--- src/main/scala/karazin/scala/users/group/week3/homework/services.scala
+++ src/main/scala/karazin/scala/users/group/week3/homework/services.scala
@@ -1,22 +1,173 @@
-//package karazin.scala.users.group.week3.homework
-//
-//import java.util.UUID
-//
-//// Do not forget to import custom implementation
-//import karazin.scala.users.group.week3.homework.adt.*
-//import karazin.scala.users.group.week3.homework.model.*
-//
-///*
-//  Dummy services
-//
-//  The services need to be implemented in case of running the code
-// */
-//object services:
-//
-//  // Implement and use ApiKey instead of String
-//  // Replace `ErrorOr[_]` with ErrorOrT[Future, _]
-//  def getUserProfile(apiKey: String): ErrorOr[UserProfile] = ???
-//  def getPosts(userId: UUID): ErrorOr[List[Post]] = ???
-//  def getComments(postId: UUID): ErrorOr[List[Comment]] = ???
-//  def getLikes(postId: UUID): ErrorOr[List[Like]] = ???
-//  def getShares(postId: UUID): ErrorOr[List[Share]] = ???
+package karazin.scala.users.group.week3.homework
+
+import karazin.scala.users.group.week3.Monad
+import karazin.scala.users.group.week3.homework.model.Post.PostId
+import karazin.scala.users.group.week3.homework.model.User.UserId
+
+import java.util.UUID
+import scala.concurrent.{ExecutionContext, Future}
+
+// Do not forget to import custom implementation
+import karazin.scala.users.group.week3.homework.adt.*
+import karazin.scala.users.group.week3.homework.model.*
+
+
+object services:
+
+  // Emulation
+  def getUserProfile(apiKey: String)
+                    (using EC: ExecutionContext): ErrorOrT[Future, UserProfile] =
+    ErrorOrT[Future, UserProfile](
+      Future {
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println("Start getting user profile...")
+
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println(s"Current thread for `getUserProfile`: ${Thread.currentThread().getName}")
+
+        Thread.sleep(2000)
+
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println("Got user profile...")
+
+        ErrorOr.Some(UserProfile(UserId.generate))
+      }(EC).recover {
+        case e: Exception =>
+          ErrorOr.Error(e)
+      }
+    )
+
+  // Emulation
+  def getPosts(userId: UserId)
+              (using EC: ExecutionContext): ErrorOrT[Future, List[Post]] =
+    ErrorOrT[Future, List[Post]](
+      Future {
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println("Start getting posts...")
+
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println(s"Current thread for `getPosts`: ${Thread.currentThread().getName}")
+
+        Thread.sleep(2000)
+
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println("Got posts...")
+
+        ErrorOr.Some(Post(userId, PostId.generate) :: Nil)
+      }(EC).recover {
+        case e: Exception =>
+          ErrorOr.Error(e)
+      }
+    )
+
+  // Emulation
+  def getComments(postId: PostId)
+                 (using EC: ExecutionContext):  ErrorOrT[Future, List[Comment]] =
+    ErrorOrT[Future, List[Comment]](
+      Future {
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println("Start getting comments...")
+
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println(s"Current thread for `getComments`: ${Thread.currentThread().getName}")
+
+        Thread.sleep(2000)
+
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println("Got comments...")
+
+        ErrorOr.Some(Comment(UserId.generate, PostId.generate) :: Nil)
+      }(EC).recover {
+        case e: Exception =>
+          ErrorOr.Error(e)
+      }
+    )
+
+  // Emulation
+  def getLikes(postId: PostId)
+              (using EC: ExecutionContext):  ErrorOrT[Future, List[Like]] =
+    ErrorOrT[Future, List[Like]](
+      Future {
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println("Start getting likes...")
+
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println(s"Current thread for `getLikes`: ${Thread.currentThread().getName}")
+
+        Thread.sleep(2000)
+
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println("Got likes...")
+
+        ErrorOr.Some(Like(UserId.generate, PostId.generate) :: Nil)
+      }(EC).recover {
+        case e: Exception =>
+          ErrorOr.Error(e)
+      }
+    )
+  def getShares(postId: PostId)
+               (using EC: ExecutionContext):  ErrorOrT[Future, List[Share]] =
+    ErrorOrT[Future, List[Share]](
+      Future {
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println("Start getting shares...")
+
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println(s"Current thread for `getShares`: ${Thread.currentThread().getName}")
+
+        Thread.sleep(2000)
+
+        /*
+         We are using `println` for simplicity.
+         Don't do this in a production ready code.
+        */
+        println("Got shares...")
+
+        ErrorOr.Some(Share(UserId.generate, PostId.generate) :: Nil)
+      }(EC).recover {
+        case e: Exception =>
+          ErrorOr.Error(e)
+      }
+    )
