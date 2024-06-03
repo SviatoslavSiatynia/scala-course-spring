efc856b55cdd7edeb2aad82a8d1c1a1eb7429559
SviatoslavSiatynia <150059661+SviatoslavSiatynia@users.noreply.github.com>
2024-06-03 17:01:56 +0300

    Week-1 (#1)
    * Week-1
    
    * Week-1: fix UserId, PostId and ApiKey types

--- src/main/scala/karazin/scala/users/group/week1/homework/adt.scala
+++ src/main/scala/karazin/scala/users/group/week1/homework/adt.scala
@@ -10,15 +10,14 @@ object adt:
   
   enum ErrorOr[+V]:
     
-    // Added to make it compilable. Remove it.
-    case DummyCase
-    
     /* 
       Two case must be defined: 
       * a case for a regular value
       * a case for an error (it should contain an actual throwable)
      */
+    case Some(v: V) extends ErrorOr[V]
   
+    case Error(e: Throwable) extends ErrorOr[Nothing]
     /* 
       The method is used for defining execution pipelines
       Provide a type parameter, an argument and a result type
@@ -26,8 +25,14 @@ object adt:
       Make sure that if an internal function is failed with an exception
       the exception is not thrown but the case for an error is returned
     */ 
-    def flatMap = ???
-
+    def flatMap[Q](f: V => ErrorOr[Q]): ErrorOr[Q] =
+      this match
+        case Error(err) => Error(err)
+        case Some(v) =>
+          try
+            f(v)
+          catch
+            case ex: Throwable => Error(ex)
     /* 
       The method is used for changing the internal object
       Provide a type parameter, an argument and a result type
@@ -35,8 +40,14 @@ object adt:
       Make sure that if an internal function is failed with an exception
       the exception is not thrown but the case for an error is returned
      */
-    def map = ???
-      
+    def map[Q](f: V => Q): ErrorOr[Q] =
+      this match
+        case Error(err) => Error(err)
+        case Some(v) =>
+          try
+            ErrorOr.Some(f(v))
+          catch
+            case ex: Throwable => Error(ex)
   // Companion object to define constructor
   object ErrorOr:
     /* 
@@ -45,6 +56,9 @@ object adt:
       Make sure that if an internal function is failed with an exception
       the exception is not thrown but the case for an error is returned
     */
-    def apply = ???
-      
+    def apply[V](expr: => V): ErrorOr[V] =
+      try
+        Some(expr)
+      catch
+        case e: Throwable => Error(e)
   
