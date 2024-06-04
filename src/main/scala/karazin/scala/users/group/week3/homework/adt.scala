3cfedaa2774a1e2439ce10e642b371117ae9cc5d
SviatoslavSiatynia <150059661+SviatoslavSiatynia@users.noreply.github.com>
2024-06-04 13:04:47 +0300

    Week-3 (#3)
    * Week-3
    
    * Week-3: remade def getPostsViews

--- src/main/scala/karazin/scala/users/group/week3/homework/adt.scala
+++ src/main/scala/karazin/scala/users/group/week3/homework/adt.scala
@@ -1,59 +1,115 @@
-//package karazin.scala.users.group.week3.homework
-//
-//object adt:
-//
-//  // Implement ErrorOrT
-//  // Prove that ErrorOrT is a monad
-//
-//  enum ErrorOr[+V]:
-//
-//    // Added to make it compilable. Remove it.
-//    case DummyCase
-//
-//    /* 
-//      Two case must be defined: 
-//      * a case for a regular value
-//      * a case for an error (it should contain an actual throwable)
-//     */
-//
-//    /* 
-//      The method is used for defining execution pipelines
-//      Provide a type parameter, an argument and a result type
-//
-//      Make sure that if an internal function is failed with an exception
-//      the exception is not thrown but the case for an error is returned
-//    */ 
-//    def flatMap = ???
-//
-//    /* 
-//      The method is used for changing the internal object
-//      Provide a type parameter, an argument and a result type
-//
-//      Make sure that if an internal function is failed with an exception
-//      the exception is not thrown but the case for an error is returned
-//     */
-//    def map = ???
-//
-//    def withFilter(p: V => Boolean) = ???
-//
-//    def flatten[U](using ev: V <:< ErrorOr[U]) = ???
-//
-//    def foreach[U](f: V => U): Unit = ???
-//
-//    def fold[Q](ifEmpty: => Q)(f: V => Q): Q = ???
-//
-//    def foldLeft[Q](z: Q)(op: (Q, V) => Q): Q = ???
-//
-//    def foldRight[Q](z: Q)(op: (V, Q) => Q): Q = ???
-//
-//
-//  // Companion object to define constructor
-//  object ErrorOr:
-//    /* 
-//      Provide a type parameter, an argument and a result type
-//
-//      Make sure that if an internal function is failed with an exception
-//      the exception is not thrown but the case for an error is returned
-//    */
-//    def apply = ???
-//
+package karazin.scala.users.group.week3.homework
+
+import karazin.scala.users.group.week3.{Functor, Monad}
+
+import scala.concurrent.Future
+
+object adt:
+
+  case class ErrorOrT[F[_], A](value: F[ErrorOr[A]]):
+    def flatMap[B](f: A => ErrorOrT[F, B])(using M: Monad[F]): ErrorOrT[F, B] =
+      ErrorOrT(M.flatMap[ErrorOr[A], ErrorOr[B]](value) {
+          case ErrorOr.Some(v) => f(v).value
+          case ErrorOr.Error(e) => M.pure(ErrorOr.Error(e))
+        }
+      )
+      
+    def map[B](f: A => B)(using Functor: Functor[F]): ErrorOrT[F, B] =
+      ErrorOrT(Functor.map(value)(v => v.map(f)))
+
+    def withFilter(p: A => Boolean)(using Functor: Functor[F]): ErrorOrT[F, A] =
+      ErrorOrT(Functor.map(value) (v => v.withFilter(p)))
+
+  enum ErrorOr[+V]:
+
+    /*
+      Two case must be defined:
+      * a case for a regular value
+      * a case for an error (it should contain an actual throwable)
+     */
+
+    case Some(v: V) extends ErrorOr[V]
+
+    case Error(e: Throwable) extends ErrorOr[Nothing]
+
+    /*
+      The method is used for defining execution pipelines
+      Provide a type parameter, an argument and a result type
+
+      Make sure that if an internal function is failed with an exception
+      the exception is not thrown but the case for an error is returned
+    */
+    def flatMap[Q](f: V => ErrorOr[Q]): ErrorOr[Q] =
+      this match
+        case Error(err) => Error(err)
+        case Some(v) =>
+          try
+            f(v)
+          catch
+            case ex: Throwable => Error(ex)
+
+    /*
+      The method is used for changing the internal object
+      Provide a type parameter, an argument and a result type
+
+      Make sure that if an internal function is failed with an exception
+      the exception is not thrown but the case for an error is returned
+     */
+    def map[Q](f: V => Q): ErrorOr[Q] =
+      this match
+        case Error(err) => Error(err)
+        case Some(v) =>
+          try
+            ErrorOr.Some(f(v))
+          catch
+            case ex: Throwable => Error(ex)
+
+    def withFilter(p: V => Boolean): ErrorOr[V] =
+      this match
+        case ErrorOr.Some(v) => if p(v) then ErrorOr.Some(v) else Error(new Exception())
+        case Error(err) => Error(err)
+
+    def flatten[U](using ev: V <:< ErrorOr[U]): ErrorOr[U] =
+      this match
+        case Error(err) => Error(err)
+        case ErrorOr.Some(v) =>
+          try
+            ev(v)
+          catch
+            case ex: Throwable => Error(ex)
+
+    def foreach[U](f: V => U): Unit =
+      this match
+        case ErrorOr.Some(v) => f(v)
+        case _ => ()
+
+    def fold[Q](ifEmpty: => Q)(f: V => Q): Q =
+      this match
+        case ErrorOr.Some(v) => f(v)
+        case _ => ifEmpty
+
+    def foldLeft[Q](z: Q)(op: (Q, V) => Q): Q =
+      this match
+        case ErrorOr.Some(v) => op(z, v)
+        case _ => z
+
+    def foldRight[Q](z: Q)(op: (V, Q) => Q): Q =
+      this match
+        case ErrorOr.Some(v) => op(v, z)
+        case _ => z
+
+
+  // Companion object to define constructor
+  object ErrorOr:
+    /*
+      Provide a type parameter, an argument and a result type
+
+      Make sure that if an internal function is failed with an exception
+      the exception is not thrown but the case for an error is returned
+    */
+    def apply[V](expr: => V): ErrorOr[V] =
+      try
+        Some(expr)
+      catch
+        case e: Throwable => Error(e)
+
--- src/main/scala/karazin/scala/users/group/week3/homework/program.scala
+++ src/main/scala/karazin/scala/users/group/week3/homework/program.scala
@@ -1,27 +1,81 @@
-//package karazin.scala.users.group.week3.homework
-//
-//// Do not forget to import custom implementation
-//import karazin.scala.users.group.week3.homework.adt.*
-//import karazin.scala.users.group.week3.homework.model.*
-//import karazin.scala.users.group.week3.homework.services.*
-//
-//object program:
-//
-//  /*
-//    Replace `ErrorOr[List[PostView]] result type with `ErrorOrT[Future, List[PostView]]
-//    Provide the implementation for the service
-//   */
-//  def getPostsViews(apiKey: String)(commentsFilter: List[Comment] => Boolean,
-//                                    likesFilter: List[Like] => Boolean): ErrorOr[List[PostView]] = ???
-//
-//  /* 
-//    Replace `ErrorOr[PostView] result type with `ErrorOrT[Future, PostView]
-//    Provide the implementation for the service
-//  */
-//  def getPostView(post: Post)(commentsFilter: List[Comment] => Boolean,
-//                              likesFilter: List[Like] => Boolean): ErrorOr[PostView] = ???
-//
-//
-//
-//
-//
+package karazin.scala.users.group.week3.homework
+
+// Do not forget to import custom implementation
+import karazin.scala.users.group.week3.Functor
+import karazin.scala.users.group.week3.future.given
+import karazin.scala.users.group.week3.homework.adt.*
+import karazin.scala.users.group.week3.homework.model.*
+import karazin.scala.users.group.week3.homework.model.Post.PostId
+import karazin.scala.users.group.week3.homework.model.User.UserId
+import karazin.scala.users.group.week3.homework.services.*
+
+import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService, Future}
+import scala.concurrent.ExecutionContext.global
+import scala.util.{Failure, Success}
+
+object program extends App:
+
+  val singleThreadPoolContext1: ExecutionContextExecutorService =
+    ExecutionContext.fromExecutorService(Executors.newSingleThreadExecutor())
+
+  val fixedTreadPoolContext1: ExecutionContextExecutorService =
+    ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(10))
+
+  val singleThreadPoolContext2: ExecutionContextExecutorService =
+    ExecutionContext.fromExecutorService(Executors.newSingleThreadExecutor())
+
+  val fixedTreadPoolContext2: ExecutionContextExecutorService =
+    ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(10))
+
+  given ExecutionContext = ExecutionContext.global
+
+  /*
+    Replace `ErrorOr[List[PostView]] result type with `ErrorOrT[Future, List[PostView]]
+    Provide the implementation for the service
+   */
+  def getPostsViews(apiKey: String)(commentsFilter: List[Comment] => Boolean,
+                                    likesFilter: List[Like] => Boolean)
+                   (using ExecutionContext): ErrorOrT[Future, List[PostView]] =
+    println(s"Main thread: ${Thread.currentThread().getName}")
+
+    val getUserProfileService = getUserProfile(apiKey)(using fixedTreadPoolContext1)
+
+    for
+      profile <- getUserProfileService
+      posts <- getPosts(profile.userId)
+      postsView <- posts.foldLeft(ErrorOrT(Future.successful(ErrorOr(List.empty[PostView])))) {
+        (views, post) => views flatMap (list => getPostView(post)(commentsFilter, likesFilter) map (list :+ _))
+      }
+    yield postsView
+
+  getPostsViews("apiKey")(_ => true, _ => true).value.onComplete {
+    case Success(value)     => println(s"Completed with $value")
+    case Failure(exception) => println(s"Failed with $exception")
+  }
+
+  Thread.sleep(10000)
+
+  singleThreadPoolContext1.shutdown()
+  fixedTreadPoolContext1.shutdown()
+
+  /*
+    Replace `ErrorOr[PostView] result type with `ErrorOrT[Future, PostView]
+    Provide the implementation for the service
+  */
+  def getPostView(post: Post)(commentsFilter: List[Comment] => Boolean,
+                              likesFilter: List[Like] => Boolean)
+                 (using ExecutionContext): ErrorOrT[Future, PostView] =
+    println(s"Main thread: ${Thread.currentThread().getName}")
+
+    val getCommentsService = getComments(post.postId)(using fixedTreadPoolContext2)
+    val getLikesService = getLikes(post.postId)(using fixedTreadPoolContext2)
+    val getSharesService = getShares(post.postId)(using fixedTreadPoolContext2)
+
+    for
+      comments <- getCommentsService
+      if commentsFilter(comments)
+      likes <- getLikesService
+      if likesFilter(likes)
+      shares <- getSharesService
+    yield PostView(post, comments, likes, shares)
